8658b6ed314b37ff9e01e04e63cf199c4d333dc3
marabesi <matheus.marabesi@gmail.com>
2016-10-24 20:06:02 -0200

    Added class 1

---
+++ Aula1/MOB.XF.Hello/MOB.XF.Hello/MOB.XF.Hello/NavHomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB.XF.Hello
+{
+    public partial class NavHomePage : ContentPage
+    {
+        public NavHomePage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnButtonClick(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new NavPage2());
+        }
+    }
+}
---
+++ Aula1/MOB.XF.Hello/MOB.XF.Hello/MOB.XF.Hello/NavPage2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB.XF.Hello
+{
+    public partial class NavPage2 : ContentPage
+    {
+        public NavPage2()
+        {
+            InitializeComponent();
+
+            Button buttonVoltar = new Button()
+            {
+                Text = "Voltar",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            buttonVoltar.Clicked += async (sender, args) =>
+            {
+                await Navigation.PopAsync();
+            };
+
+            this.rootLayout.Children.Add(buttonVoltar);
+        }
+    }
+}
