2eb1d550b8debda8ed569f8fc781340324eceb0d
marabesi <matheus.marabesi@gmail.com>
2016-10-24 22:51:43 -0200

    added mvvm project

---
+++ MVVM/MVVM/MVVM/StudentViewPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    public partial class StudentViewPage : ContentPage
+    {
+        StudentViewModel studentViewModel;
+
+        public StudentViewPage()
+        {
+            var student = StudentViewModel.GetStudent();
+            studentViewModel = new StudentViewModel(student);
+            BindingContext = studentViewModel;
+
+            InitializeComponent();
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs args)
+        {
+            var selectedStudent = args.Item as Student;
+            DisplayAlert("Selected studet", string.Format("{0}", selectedStudent.Id), "OK");
+        }
+    }
+}
