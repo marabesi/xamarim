e74c39b648a2dd8e0540836aaa77e2aea0063e36
marabesi <matheus.marabesi@gmail.com>
2017-01-01 19:59:44 -0200

    changed the port to handle the requests

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(methodOverride());
 
 var user = process.env.DB_USER,
     password = process.env.DB_PASSWORD,
-    host = process.env.DB_HOST;
+    host = process.env.DB_HOST,
+    port = process.env.PORT || 8000;
 
 mongoose.connect("mongodb://" + user + ":" + password + "@" + host);
 
@@ -61,5 +62,5 @@ var Sensors = app.sensors = restful.model('sensors', mongoose.Schema({
 
 Sensors.register(app, '/sensors');
 
-app.listen(8001, '0.0.0.0');
+app.listen(port);
 
